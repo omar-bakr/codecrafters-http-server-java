9e0c58b22ee67ad42c24e59e9a3432b3ece0a46c
omar bakr <omar.bakr.ext@siemens.com>
2025-04-16 12:11:09 +0200

    Parsing the path from the request

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,13 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,7 +21,17 @@ public class Main {
             serverSocket.setReuseAddress(true);
 
             Socket socket = serverSocket.accept();
-            socket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+            InputStream inputStream = socket.getInputStream();
+            String request = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+            String[] requestSeparated = request.split("\r\n");
+            String[] requestLines = requestSeparated[0].split(" ");
+            String path = requestLines[1];
+            if (path.equals("/")) {
+                socket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+            } else {
+                socket.getOutputStream().write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
+            }
+
             System.out.println("accepted new connection");
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());

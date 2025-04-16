5d88d70aea881adefc2e519f17458f124d347de7
omar bakr <omar.bakr.ext@siemens.com>
2025-04-16 21:15:03 +0200

    Another try to fix things

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,16 +15,15 @@ public class Main {
     private static final String USER_AGENT_KEY = "User-Agent";
 
 
-
     public static void main(String[] args) {
         // You can use print statements as follows for debugging, they'll be visible when running tests.
         System.out.println("Logs from your program will appear here!");
 
-        while (true) {
-            try (ServerSocket serverSocket = new ServerSocket(4221)) {
-                // Since the tester restarts your program quite often, setting SO_REUSEADDR
-                // ensures that we don't run into 'Address already in use' errors
-                serverSocket.setReuseAddress(true);
+        try (ServerSocket serverSocket = new ServerSocket(4221)) {
+            // Since the tester restarts your program quite often, setting SO_REUSEADDR
+            // ensures that we don't run into 'Address already in use' errors
+            serverSocket.setReuseAddress(true);
+            while (true) {
                 Socket socket = serverSocket.accept();
 
                 HttpRequest request = parseHttpRequest(socket.getInputStream());//Parsing the data
@@ -41,12 +40,11 @@ public class Main {
                 } else {
                     socket.getOutputStream().write(buildResponse(404, "Not Found").getBytes());
                 }
-
-                System.out.println("accepted new connection");
-            } catch (IOException e) {
-                System.out.println("IOException: " + e.getMessage());
             }
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
         }
+
     }
 
     private static HttpRequest parseHttpRequest(InputStream inputStream) throws IOException {
